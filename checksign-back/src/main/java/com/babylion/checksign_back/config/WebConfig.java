08e665af99ae31c72af4fd9dac9a1c3aea3aab27
Dongjun Park <pdj5096@gmail.com>
2025-06-23 02:24:24 +0900

    Update WebConfig.java

--- checksign-back/src/main/java/com/babylion/checksign_back/config/WebConfig.java
+++ checksign-back/src/main/java/com/babylion/checksign_back/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://checksign.pdj.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
